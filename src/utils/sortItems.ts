d026864c6fd033c650b4a92b0ac1474646a3b9ea
Kristina Petrova <kristina.petrova@homebuddy.com>
2026-02-27 15:35:14 +0600

    fix mutability

--- src/utils/sortItems.ts
+++ src/utils/sortItems.ts
@@ -2,16 +2,17 @@ import type {Sort, Task} from "./types";
 import {PRIORITY_ORDER} from "./constants";
 
 export const sortItems = (tasks: Task[], sort: Sort) => {
+    const sorted = [...tasks];
     switch (sort) {
         case "newest":
-            return tasks.sort((a, b) => b.date - a.date);
+            return sorted.sort((a, b) => b.date - a.date);
         case "oldest":
-            return tasks.sort((a, b) => a.date - b.date);
+            return sorted.sort((a, b) => a.date - b.date);
         case "inc":
-            return tasks.sort((a, b) => PRIORITY_ORDER[a.priority] - PRIORITY_ORDER[b.priority]);
+            return sorted.sort((a, b) => PRIORITY_ORDER[a.priority] - PRIORITY_ORDER[b.priority]);
         case "dec":
-            return tasks.sort((a, b) => PRIORITY_ORDER[b.priority] - PRIORITY_ORDER[a.priority]);
+            return sorted.sort((a, b) => PRIORITY_ORDER[b.priority] - PRIORITY_ORDER[a.priority]);
         default:
-            return tasks.sort((a, b) => b.date - a.date);
+            return sorted;
     }
 }
